802929c96f820cb04990ba2a5e15c9eeffe45b95
StephanieDCh <111522234+StephanieDCh@users.noreply.github.com>
2022-10-03 22:04:36 -0500

    Se agregan archivos de código de otros-3

---
+++ codigo-otros-3/filtro.js
@@ -0,0 +1,56 @@
+
+const productos = [
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+]
+const li = document.getElementById("lista-de-productos")
+//se manda llamar el id del HTML con getElementById
+const $i = document.querySelector('.input');
+
+
+function displayProductos(productos) {//se mejora la función con esta
+    
+  for (let contador = 0; contador < productos.length; contador++) {
+    let zapato =`<div class ="producto">
+    <p class="titulo">${productos[contador].nombre}</p>
+    <img src=${productos[contador].img}></img></div>`
+    li.innerHTML+= zapato;  
+  }//for
+}//funcion dP
+
+displayProductos(productos)
+const botonDeFiltro = document.querySelector("button");
+
+botonDeFiltro.onclick = function() {
+  while (li.firstChild) {
+    li.removeChild(li.firstChild);
+  }
+
+  const texto = $i.value;
+  console.log(texto);
+  const productosFiltrados = filtrado(productos, texto );
+
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
+  
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productosFiltrados[i].nombre
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productosFiltrados[i].img);
+  
+    d.appendChild(ti)
+    d.appendChild(imagen)
+  
+    li.appendChild(d)
+  }
+}
+
+const filtrado = (productos = [], texto) => {
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+}  
